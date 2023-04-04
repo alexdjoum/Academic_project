c1acd889616017b52025d0d0b2428adaf12cc73e
alexmartial <kammegnealex@gmail.com>
2023-04-04 12:30:24 +0100

    I just add anything

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -12,5 +12,20 @@
     @endforelse
 </ul>
 
+<ul>
+    @foreach ($events as $event)
+        <li>
+            {{ $event }}
+            @if (strpos($event,'ANDROID') !== false)
+                (sweet framework!)
+                @elseif (strpos($event, 'Symphony') !== false)
+                    (Sweet symphony is an old framework)
+                @else (strpos($event, 'Laravel') !== false)
+                    (I am using Laravel)
+            @endif
+        </li>
+    @endforeach
+</ul>
+
 <img src="/img/tennis1.jpeg">
 <img src="{{ asset('img/tennis1.jpeg') }}">
