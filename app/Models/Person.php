b1443a23688e54e5772e58cfdb349fb5e71e2449
alexmartial <kammegnealex@gmail.com>
2023-06-06 11:09:49 +0100

    Training with models

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Person extends Model
 {
     use HasFactory;
+    public function setPasswordAttribute($password)
+    {
+        $this->attributes['password'] = bcrypt($password);
+    }
 }
